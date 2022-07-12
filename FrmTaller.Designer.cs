661c9660fada74836de70bc69d5e759ccd014b59
Roberto Cordero Quirós <RC4691CP@grupomonge.com>
2022-07-12 15:38:34 -0600

    Agregar archivos de proyecto.

---
+++ FrmTaller.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TallerGrupoMongeUNI
+{
+    partial class FrmTaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListas = new System.Windows.Forms.Button();
+            this.btnVariable = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnListas
+            // 
+            this.btnListas.Location = new System.Drawing.Point(138, 42);
+            this.btnListas.Name = "btnListas";
+            this.btnListas.Size = new System.Drawing.Size(75, 23);
+            this.btnListas.TabIndex = 0;
+            this.btnListas.Text = "Listas";
+            this.btnListas.UseVisualStyleBackColor = true;
+            this.btnListas.Click += new System.EventHandler(this.btnListas_Click);
+            // 
+            // btnVariable
+            // 
+            this.btnVariable.Location = new System.Drawing.Point(42, 42);
+            this.btnVariable.Name = "btnVariable";
+            this.btnVariable.Size = new System.Drawing.Size(75, 23);
+            this.btnVariable.TabIndex = 1;
+            this.btnVariable.Text = "Variables";
+            this.btnVariable.UseVisualStyleBackColor = true;
+            this.btnVariable.Click += new System.EventHandler(this.btnVariable_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(26, 308);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 15);
+            this.lblError.TabIndex = 2;
+            // 
+            // FrmTaller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 347);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnVariable);
+            this.Controls.Add(this.btnListas);
+            this.Name = "FrmTaller";
+            this.Text = "FrmTaller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListas;
+        private System.Windows.Forms.Button btnVariable;
+        private System.Windows.Forms.Label lblError;
+    }
+}
---
+++ FrmTaller.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TallerGrupoMongeUNI
+{
+    public partial class FrmTaller : Form
+    {
+
+        public LogicaNegocio.Variable objVariable = new LogicaNegocio.Variable();
+
+        private string _logErrores = string.Empty;
+
+        public FrmTaller()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void btnListas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnVariable_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    var pruebaError = 0;
+                    var resultadoOperacion = 5 / pruebaError;
+                }
+                catch (DivideByZeroException ex)
+                {
+                    _logErrores = _logErrores + "/ " + ex.Message;
+                    lblError.Text = _logErrores;
+                }
+                var referencia = "Prueba Referencia ";
+                var resultado = objVariable.ObtenerContador(ref referencia);
+                MessageBox.Show("Valor de Referencia: " + referencia); 
+                MessageBox.Show("Se obtiene contador de clase: " + resultado);
+                var resultadoEstatico = LogicaNegocio.VariableEstatica.ObtenerContador();
+                MessageBox.Show("Se obtiene contador de clase Estatica: " + resultadoEstatico);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                //throw ex;
+            }
+
+
+        }
+    }
+}
